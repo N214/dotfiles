01ed10d7971a13a0679d8ddceb7a51c3c1cc1596
Nicolas Tang <nico9156@gmail.com>
2023-12-25 14:46:14 -0500

    change neovim config

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+-- If you'd rather extend the default config, use the code below instead:
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add tsx and treesitter
+    vim.list_extend(opts.ensure_installed, {
+      "tsx",
+      "typescript",
+    })
+  end,
+}
